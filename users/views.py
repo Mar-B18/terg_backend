a8455cc8e570e83c34a2b2f03c0a1a478ba3f1b5
William C Kohe <williamckohe@gmail.com>
2025-04-21 11:30:36 +0300

    rest api endpoints and views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from users.serializers import UserSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+class RegisterView(APIView):
+    permission_classes = []
+
+    def post(self, request):
+       try:
+           data = request.data
+           serializer =  UserSerializer(data=data)
+           if serializer.is_valid():
+            serializer.save()
+            return Response({
+               "Message" : "User Created Successfully",
+               "error" : False,
+            }, status=status.HTTP_201_CREATED)
+           else:
+            return Response({
+               "Message" : "An error occurred",
+               "error" : True,
+            },serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+       except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    
